d38d2bb664a77e0a773e5b2c3d63874c751f84ce
shevernitskiy <hwork@list.ru>
2017-11-15 12:22:42 +0300

    main code

---
+++ lib/config/plugin.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'name'        => _w('EMS'),
+    'description' => _w('EMS international courier shipping'),
+    'icon'        => 'img/Alien.png',
+    'logo'        => 'img/ems.png',
+    'version'     => '1.0.0',
+    'vendor'      => 'shevernitskiy',
+);
---
+++ lib/model/shippingRuems.model.php
@@ -0,0 +1,7 @@
+<?php
+
+class shippingRuemsModel extends waModel
+{
+    protected $table = 'wa_shipping_rupost_tracking';
+
+}
